3d1a3bf009d00279619846aa0d4266ad8b1c7661
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2020-06-26 19:35:44 -0700

    Update basic map, tutorial window

--- Assets/Scripts/IntroDisplay.cs
+++ Assets/Scripts/IntroDisplay.cs
@@ -6,16 +6,28 @@ public class IntroDisplay : MonoBehaviour
 {
     [SerializeField] Canvas relatedIntro = null;
     [SerializeField] CameraMovement cameraControl = null;
+    [SerializeField] Texture2D cursorTexture = null;
+
+    //called when mouse hover onto the object
     private void OnMouseOver()
     {
         Debug.Log("Mouse entered");
         if (relatedIntro == null) return;
+        if (cursorTexture != null)
+        {
+            Cursor.SetCursor(cursorTexture, new Vector2(), CursorMode.Auto);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Clicked");
             cameraControl.enabled = false;
             relatedIntro.enabled = true;
         }
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(null, new Vector2(), CursorMode.Auto);
     }
 
     public void CloseIntro()
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    
+    [SerializeField] float waitTime = 3f;
+    [SerializeField] CameraMovement cameraControl = null;
+    
+    [SerializeField] GameObject tutorialButton;
+    [SerializeField] GameObject directStartButton;
+
+    Text introText = null;
+    Image backgroundImage = null;
+    CanvasGroup canvasGroup = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraControl.enabled = false;
+        introText = GetComponentInChildren<Text>();
+        backgroundImage = GetComponentInChildren<Image>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnTutorialStartClicked()
+    {
+        HideButtons();
+        StartCoroutine(TutorialFunction());
+    }
+
+    public void OnDirectStartClicked()
+    {
+        HideButtons();
+        StartCoroutine(DirectStartFunction());
+    }
+
+    private void HideButtons()
+    {
+        tutorialButton.SetActive(false);
+        directStartButton.SetActive(false);
+    }
+
+    private IEnumerator TutorialFunction()
+    {
+       
+        introText.text = "按住鼠标左键，并移动鼠标来移动镜头位置";
+        yield return new WaitForSeconds(waitTime);
+        introText.text = "按住鼠标右键，并左右移动鼠标来旋转镜头";
+        yield return new WaitForSeconds(waitTime);
+        introText.text = "现在，开始游览西塞姆利亚大陆吧！";
+
+        cameraControl.enabled = true;
+        yield return FadeOut(waitTime);
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator DirectStartFunction()
+    {
+        cameraControl.enabled = true;
+        yield return FadeOut(waitTime);
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator FadeOut(float time)
+    {
+        while (!Mathf.Approximately(0f, canvasGroup.alpha))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, 0f, Time.deltaTime / time);
+            yield return null;
+        }
+    }
+}
