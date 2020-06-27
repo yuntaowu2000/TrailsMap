9eb8606aef91f074a2b76f9e386032f99809ae33
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2020-06-27 10:29:01 -0700

    Fixed raycasting issue

--- Assets/Scripts/IntroDisplay.cs
+++ Assets/Scripts/IntroDisplay.cs
@@ -8,10 +8,19 @@ public class IntroDisplay : MonoBehaviour
     [SerializeField] CameraMovement cameraControl = null;
     [SerializeField] Texture2D cursorTexture = null;
 
+    IntroDisplay[] allIntroDisplays = null;
+    bool opened = false;
+
+    private void Start()
+    {
+        allIntroDisplays = FindObjectsOfType<IntroDisplay>();
+    }
+
     //called when mouse hover onto the object
     private void OnMouseOver()
     {
         Debug.Log("Mouse entered");
+        if (opened) return;
         if (relatedIntro == null) return;
         if (cursorTexture != null)
         {
@@ -20,8 +29,22 @@ public class IntroDisplay : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Clicked");
+            opened = true;
             cameraControl.enabled = false;
             relatedIntro.enabled = true;
+            SetOtherIntroDisplay(false);
+        }
+    }
+
+    private void SetOtherIntroDisplay(bool active)
+    {
+        foreach (IntroDisplay introDisplay in allIntroDisplays)
+        {
+            if (introDisplay != this)
+            {
+                introDisplay.enabled = active;
+                introDisplay.gameObject.GetComponent<Collider>().enabled = active;
+            }
         }
     }
 
@@ -32,7 +55,9 @@ public class IntroDisplay : MonoBehaviour
 
     public void CloseIntro()
     {
+        opened = false;
         cameraControl.enabled = true;
         relatedIntro.enabled = false;
+        SetOtherIntroDisplay(true);
     }
 }
