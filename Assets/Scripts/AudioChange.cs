e377a464955957b3df788c69be1a39ad67016e6c
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-04-22 21:21:52 -0700

    Update BGM selection

--- Assets/Scripts/AudioChange.cs
+++ Assets/Scripts/AudioChange.cs
@@ -7,73 +7,95 @@ using UnityEngine.Networking;
 
 public class AudioChange : MonoBehaviour
 {
-    string musicVersion = null;
-    string[] musicNames = {"wind_from_liberl.mp3", "116_Water_Plants_and_the_Blue_Sky.mp3", "1-21_Beyond_the_Drifting_Clouds.mp3", "ed9999.mp3"};
-    AudioSource audioSource;
+    // string musicVersion = null;
+    // string[] musicNames = {"wind_from_liberl.mp3", "116_Water_Plants_and_the_Blue_Sky.mp3", "1-21_Beyond_the_Drifting_Clouds.mp3", "ed9999.mp3"};
+    private AudioSource audioSource;
+    [SerializeField] private Text BGMText = null;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-    }
-
-    public void SetVersion() {
-        //get the name of the button and set it as destination scene
-        string newMusicVersion = EventSystem.current.currentSelectedGameObject.name;
-        if (newMusicVersion == musicVersion) return;
-        musicVersion = newMusicVersion;
-        switch (musicVersion)
-        {
-            case "Sora": 
-                StartCoroutine(GetBGM(musicNames[0]));
-                Debug.Log("Sora clicked");
-                break;
-            case "Ao":
-                StartCoroutine(GetBGM(musicNames[1]));
-                Debug.Log("Ao clicked");
-                break;
-            case "Sen":
-                StartCoroutine(GetBGM(musicNames[2]));
-                Debug.Log("Sen clicked");
-                break;
-            case "Akatsuki":
-                StartCoroutine(GetBGM(musicNames[3]));
-                Debug.Log("test clicked");
-                break;
-            default:
-                StartCoroutine(GetBGM(musicNames[0]));
-                break;
+        if (BGMText != null) {
+            BGMText.text = string.Format("BGM: None");
         }
     }
 
     public void SetSilent()
     {
         audioSource.Stop();
+        if (BGMText != null) {
+            BGMText.text = string.Format("BGM: None");
+        }
     }
 
-    private IEnumerator GetBGM(string musicName) {
-        string url = string.Format("https://data.trails-game.com/musics/{0}", musicName);
+    public void SetBGM(int dropdownValue) {
+        if (dropdownValue == 0) {
+            if (BGMText != null) {
+                BGMText.text = string.Format("BGM: None");
+            }
+            if (audioSource.isPlaying) {
+                audioSource.Stop();
+            }
+            return;
+        }
+        StartCoroutine(GetBGM(dropdownValue));
+    }
+
+    private IEnumerator GetBGM(int dropdownValue) {
+        string url = string.Format("https://data.trails-game.com/musics/{0}.mp3", dropdownValue);
         Debug.Log(url);
 
         using (var uwr = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG)) {
-
+            
             DownloadHandlerAudioClip dlHandler = (DownloadHandlerAudioClip)uwr.downloadHandler;
             dlHandler.streamAudio = true;
 
-            yield return uwr.SendWebRequest();
+            var operation = uwr.SendWebRequest();
+
+            while (!operation.isDone) {
+                BGMText.text = string.Format("BGM: Downloading {0:0}%", uwr.downloadProgress * 100);
+                yield return null;
+            }
             
             if (uwr.isNetworkError || uwr.isHttpError) {
                 Debug.Log(uwr.error);
                 yield break;
             }
 
-            if (dlHandler.isDone) {
-                AudioClip audioClip = dlHandler.audioClip;
-                if (audioClip != null) {
-                    audioSource.clip = DownloadHandlerAudioClip.GetContent(uwr);
-                    audioSource.Play();
-                }
+            audioSource.clip = DownloadHandlerAudioClip.GetContent(uwr);
+            audioSource.Play();
+            if (BGMText != null) {
+                BGMText.text = string.Format("BGM: Playing");
             }
-
         }
 
     }
+
+    // public void SetVersion() {
+    //     //get the name of the button and set it as destination scene
+    //     string newMusicVersion = EventSystem.current.currentSelectedGameObject.name;
+    //     if (newMusicVersion == musicVersion) return;
+    //     musicVersion = newMusicVersion;
+    //     switch (musicVersion)
+    //     {
+    //         case "Sora": 
+    //             StartCoroutine(GetBGM(musicNames[0]));
+    //             Debug.Log("Sora clicked");
+    //             break;
+    //         case "Ao":
+    //             StartCoroutine(GetBGM(musicNames[1]));
+    //             Debug.Log("Ao clicked");
+    //             break;
+    //         case "Sen":
+    //             StartCoroutine(GetBGM(musicNames[2]));
+    //             Debug.Log("Sen clicked");
+    //             break;
+    //         case "Akatsuki":
+    //             StartCoroutine(GetBGM(musicNames[3]));
+    //             Debug.Log("test clicked");
+    //             break;
+    //         default:
+    //             StartCoroutine(GetBGM(musicNames[0]));
+    //             break;
+    //     }
+    // }
 }
